ff04984802546494154a2de62ef89244e0b80342
Nandini Monpara <nandinimonpara1@gmail.com>
2026-04-25 15:05:49 +0530

    Update server URL for TestDino configuration

--- playwright.config.js
+++ playwright.config.js
@@ -23,7 +23,7 @@ export default defineConfig({
     ['@testdino/playwright', {
       token: process.env.TESTDINO_TOKEN,
       debug: true,
-      serverUrl: 'https://testdino-backend-uat.purplegrass-beecf167.eastus.azurecontainerapps.io',
+      serverUrl: 'https://stg-api.testdino.com',
     }],
   ],
 
